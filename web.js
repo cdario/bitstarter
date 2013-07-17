55c72a0bd2b93144a11ba22aca80b3ab41f5ea50
cd.velandia <cd.velandia@gmail.com>
2013-07-17 09:21:58 +0000

    fixed 2 web.js

--- web.js
+++ web.js
@@ -3,11 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var fs = require('fs');
+
 var buff = new Buffer(5);
 
-var buff = fs.readFile("index.html");
+var mymessage  = fs.readFile("index.html", 'utf-8');
 
-var mymessage = buff.toString('utf-8');
+/*var mymessage = buff.toString('utf-8');*/
 
 
 app.get('/', function(request, response) {
