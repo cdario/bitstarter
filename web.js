a02cea04fd7cca47f7eef8640b42f946b6c57ac4
cd.velandia <cd.velandia@gmail.com>
2013-07-17 09:56:24 +0000

    fix Buffer instance

--- web.js
+++ web.js
@@ -4,11 +4,13 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
+var buf = new Buffer(fs.readFile("index.html"));
+
 /*var mymessage = fs.readFile("index.html").toString('utf-8');*/
 
-var mymessage  = fs.readFile("index.html", 'utf-8');
+/*var mymessage  = fs.readFile("index.html", 'utf-8');*/
 
-/*var mymessage = buff.toString('utf-8');*/
+var mymessage = buf.toString('utf-8');
 
 
 app.get('/', function(request, response) {
